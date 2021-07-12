ab2ecf8ee02dba757288f192c206a5baa83f6100
youngho-j <alfkwl1239@naver.com>
2021-07-12 22:38:32 +0900

    DB 정보 properties로 이관, test 수정

--- mvcPractice02/src/test/java/com/rebuild/test/DBConnectionTest.java
+++ mvcPractice02/src/test/java/com/rebuild/test/DBConnectionTest.java
@@ -7,11 +7,13 @@ import javax.sql.DataSource;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/root-context.xml"})
 public class DBConnectionTest {
 	
 	@Inject
