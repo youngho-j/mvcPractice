e8113f86978e79535eb88e93c7fbc52163c86fd0
youngho-j <alfkwl1239@naver.com>
2021-11-29 22:38:00 +0900

    검색 기능 구현 (진행중)
    1. BookMapper 상품 목록 및 상품 수 쿼리 및 테스트 코드 작성

---
+++ mvcPractice04/src/main/java/com/spring/shop/mapper/BookMapper.java
@@ -0,0 +1,15 @@
+package com.spring.shop.mapper;
+
+import java.util.List;
+
+import com.spring.shop.util.PageInfo;
+import com.spring.shop.vo.BookVO;
+
+public interface BookMapper {
+	
+	// 상품 목록
+	public List<BookVO> goodsGetList(PageInfo pageInfo);
+	
+	// 상품 전체 갯수
+	public int goodsGetTotal(PageInfo pageInfo);
+}
---
+++ mvcPractice04/src/test/java/com/spring/shop/mapper/BookMapperTest.java
@@ -0,0 +1,43 @@
+package com.spring.shop.mapper;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.*;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.spring.shop.util.PageInfo;
+import com.spring.shop.vo.BookVO;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+public class BookMapperTest {
+	
+	@Autowired
+	private BookMapper bookMapper;
+	
+	@Test
+	public void 상품목록_리턴_테스트() throws Exception {
+		List<BookVO> list = bookMapper.goodsGetList(new PageInfo(1, 10));
+		
+		log.info(list.toString());
+		
+		assertFalse(list.isEmpty());
+	}
+	
+	@Test
+	public void 상품_전체_갯수_리턴_테스트() throws Exception {
+		int result = bookMapper.goodsGetTotal(new PageInfo(1, 10));
+		
+		assertTrue(result > 0);
+	}
+	
+}
