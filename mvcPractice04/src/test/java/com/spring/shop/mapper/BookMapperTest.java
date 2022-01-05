33d1dd904cc255dcbc96249071dcba4ed8760708
youngho-j <alfkwl1239@naver.com>
2022-01-05 23:18:05 +0900

    수정 사항
    1. BookMapper 수정
     - 작가 목록이 빈값 혹은 null일 경우 NULLIF 함수를 사용하도록 수정
     - 키워드와 일치하는 경우의 결과를 모두 출력하도록 기존 검색인 and 조건을 or 조건으로 변경
    
    2. BookMapper 수정
     - 검색 조건에 작가가 포함될 경우 작가 목록 검색후 파라미터에 추가하도록 테스트 코드 변경

--- mvcPractice04/src/test/java/com/spring/shop/mapper/BookMapperTest.java
+++ mvcPractice04/src/test/java/com/spring/shop/mapper/BookMapperTest.java
@@ -3,7 +3,6 @@ package com.spring.shop.mapper;
 import static org.junit.Assert.*;
 import static org.hamcrest.core.Is.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
@@ -24,39 +23,6 @@ public class BookMapperTest {
 	private BookMapper bookMapper;
 	
 	@Test
-	public void 작가_제목_검색_목록_리턴_테스트() throws Exception {
-		PageInfo info = new PageInfo(1, 10);
-		info.setType("T");
-		info.setKeyword("한");
-		
-		List<BookVO> list = bookMapper.getGoodsList(info);
-		
-		assertThat(2, is(list.size()));
-	}
-	
-	@Test
-	public void 작가_잘못된_검색_목록_리턴_테스트() throws Exception {
-		PageInfo info = new PageInfo(1, 10);
-		info.setType("A");
-		info.setKeyword("활");
-		info.setAuthorIdList(new ArrayList<String>());
-		
-		List<BookVO> list = bookMapper.getGoodsList(info);
-		
-		assertThat(0, is(list.size()));
-	}
-	
-	@Test
-	public void 제목검색_상품_전체_갯수_리턴_테스트() throws Exception {
-		PageInfo info = new PageInfo(1, 10);
-		info.setType("T");
-		info.setKeyword("한");
-		int result = bookMapper.getGoodsTotal(info);
-		
-		assertTrue(result > 0);
-	}
-	
-	@Test
 	public void 작가목록에_존재하지_않는_작가_아이디_목록_리턴_테스트() throws Exception {
 		List<String> list = bookMapper.getAuthorIdList("1 or 1=1");
 		
@@ -69,6 +35,74 @@ public class BookMapperTest {
 		
 		assertThat(2, is(list.size()));
 	}
+	
+	@Test
+	public void 키워드_제목_검색_목록_리턴_테스트() throws Exception {
+		PageInfo info = new PageInfo(1, 10);
+		info.setType("T");
+		info.setKeyword("한");
+		
+		List<BookVO> list = bookMapper.getGoodsList(info);
+		
+		assertThat(2, is(list.size()));
+	}
+	
+	@Test
+	public void 잘못된_키워드_작가_검색_목록_리턴_테스트() throws Exception {
+		PageInfo info = new PageInfo(1, 10);
+		info.setType("A");
+		info.setKeyword("-");
+		
+		List<String> authorList = bookMapper.getAuthorIdList(info.getKeyword());
+		
+		info.setAuthorIdList(authorList);
+		
+		List<BookVO> list = bookMapper.getGoodsList(info);
+		
+		assertThat(0, is(list.size()));
+	}
+	
+	@Test
+	public void 작가_검색_목록_리턴_테스트() throws Exception {
+		PageInfo info = new PageInfo(1, 10);
+		info.setType("A");
+		info.setKeyword("신");
+		
+		List<String> authorList = bookMapper.getAuthorIdList(info.getKeyword());
+		
+		info.setAuthorIdList(authorList);
+		
+		List<BookVO> list = bookMapper.getGoodsList(info);
+		
+		assertThat(2, is(list.size()));
+	}
+	
+	@Test
+	public void 제목_작가_검색_목록_리턴_테스트() throws Exception {
+		PageInfo info = new PageInfo(1, 10);
+		info.setType("AT");
+		info.setKeyword("한");
+		
+		List<String> authorList = bookMapper.getAuthorIdList(info.getKeyword());
+		
+		info.setAuthorIdList(authorList);
+		
+		List<BookVO> list = bookMapper.getGoodsList(info);
+		
+		assertThat(2, is(list.size()));
+	}
+	
+	@Test
+	public void 제목_키워드_상품_전체_개수_리턴_테스트() throws Exception {
+		PageInfo info = new PageInfo(1, 10);
+		info.setType("T");
+		info.setKeyword("한");
+		
+		int result = bookMapper.getGoodsTotal(info);
+		
+		assertTrue(result > 0);
+	}
+	
 	
 	@Test
 	public void 국내_카테고리_목록_리턴_테스트() throws Exception {
