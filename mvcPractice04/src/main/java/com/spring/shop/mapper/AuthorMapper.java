e8e0e9c27a8d39ebe9de1955e16ed27a2ecbcde0
youngho-j <alfkwl1239@naver.com>
2022-01-08 15:08:05 +0900

    수정 사항
    1. AuthorMapper.xml 수정
     - 테스트용 쿼리 추가
    
    2. AuthorMapper.java 수정
     - 테스트용 쿼리 추가
    
    3. AuthorMapperTest 수정
     - 테스트 코드 수정

--- mvcPractice04/src/main/java/com/spring/shop/mapper/AuthorMapper.java
+++ mvcPractice04/src/main/java/com/spring/shop/mapper/AuthorMapper.java
@@ -8,23 +8,32 @@ import com.spring.shop.vo.AuthorVO;
 public interface AuthorMapper {
 	
 	// 작가 등록
-	public int authorEnroll(AuthorVO authorVO) throws Exception;
+	public int authorEnroll(AuthorVO authorVO);
 	
 	// 작가 목록
-	public List<AuthorVO> authorGetList(PageInfo paging) throws Exception;
+	public List<AuthorVO> authorGetList(PageInfo paging);
 	
 	// 등록된 작가 인원 수
-	public int authorGetTotal(PageInfo paging) throws Exception;
+	public int authorGetTotal(PageInfo paging);
 
 	// 작가 상세
-	public AuthorVO authorGetDetail(int authorId) throws Exception;
+	public AuthorVO authorGetDetail(int authorId);
 	
 	// 작가 정보 수정
-	public int authorModify(AuthorVO authorVO) throws Exception;
+	public int authorModify(AuthorVO authorVO);
 	
 	// 작가 정보 삭제
-	public int authorDelete(int authorId) throws Exception;
+	public int authorDelete(int authorId);
 	
 	// 작가의 ID로 등록된 책의 수
-	public int authorWrittenBook(int authorId) throws Exception;
+	public int authorWrittenBook(int authorId);
+	
+	// 테이블 전체 삭제(테스트용)
+	public void deleteAll();
+	
+	// 테이블 전체 삭제 검증(테스트용)
+	public int getCount();
+	
+	// 마지막으로 등록한 작가 정보(테스트용)
+	public int getLastPK();
 }
--- mvcPractice04/src/test/java/com/spring/shop/mapper/AuthorMapperTest.java
+++ mvcPractice04/src/test/java/com/spring/shop/mapper/AuthorMapperTest.java
@@ -5,97 +5,184 @@ import static org.hamcrest.core.Is.*;
 
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.spring.shop.util.PageInfo;
 import com.spring.shop.vo.AuthorVO;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/test-root-context.xml")
 public class AuthorMapperTest {
 	
 	@Autowired
 	private AuthorMapper authorMapper;
 	
+	private AuthorVO author1;
+	private AuthorVO author2;
+	private AuthorVO author3;
+	
+	@Before
+	public void setUp() {
+		author1 = new AuthorVO();
+		author2 = new AuthorVO();
+		author3 = new AuthorVO();
+		
+		
+		author1.setNationId("01");
+		author1.setAuthorName("테스트1");
+		author1.setAuthorProfile("테스터입니다.");
+		
+		author2.setNationId("02");
+		author2.setAuthorName("테스트2");
+		author2.setAuthorProfile("테스터입니다.");
+		
+		author3.setNationId("23534654365443653968450968402437329847");
+		author3.setAuthorName("테스트1");
+		author3.setAuthorProfile("테스터입니다.");
+	}
+	
+	@Test
+	public void getCount_메서드_테스트() throws Exception {
+		authorMapper.deleteAll();
+		assertThat(authorMapper.getCount(), is(0));
+		
+		authorMapper.authorEnroll(author1);
+		assertThat(authorMapper.getCount(), is(1));
+		
+		authorMapper.authorEnroll(author2);
+		assertThat(authorMapper.getCount(), is(2));
+	}
+	
 	@Test
 	public void 작가_등록_메서드_테스트() throws Exception {
-		AuthorVO authorVO = new AuthorVO();
+		authorMapper.deleteAll();
 		
-		authorVO.setNationId("02");
-		authorVO.setAuthorName("프로도");
-		authorVO.setAuthorProfile("프로도에요");
+		assertThat(authorMapper.getCount(), is(0));
 		
-		log.info(authorVO.toString());
-		
-		int result = authorMapper.authorEnroll(authorVO);
+		int result = authorMapper.authorEnroll(author1);
 		
 		assertThat(1, is(result));
+		
+		assertThat(authorMapper.getCount(), is(1));
+	}
+	
+	@Test(expected = DataIntegrityViolationException.class)
+	public void 작가_등록시_컬럼값_초과_예외처리_테스트() throws Exception {
+		
+		authorMapper.deleteAll();
+		
+		assertThat(authorMapper.getCount(), is(0));
+		
+		authorMapper.authorEnroll(author3);
 	}
 
 	@Test
 	public void 작가목록_출력_메서드_테스트() throws Exception {
-		PageInfo paging = new PageInfo(2, 10);
+		PageInfo paging = new PageInfo(1, 10);
 		
-		List<AuthorVO> list = authorMapper.authorGetList(paging);
+		authorMapper.deleteAll();
 		
-		assertNotNull(list);
+		assertThat(authorMapper.getCount(), is(0));
 		
-		log.info(list.toString());
+		authorMapper.authorEnroll(author1);
+		authorMapper.authorEnroll(author2);
 		
+		assertThat(authorMapper.getCount(), is(2));
+		
+		List<AuthorVO> list = authorMapper.authorGetList(paging);		
+		
+		assertFalse(list.isEmpty());
+		assertThat(list.size(), is(2));
 	}
 	
 	@Test
 	public void 등록된_작가수_카운팅_메서드_테스트() throws Exception {
 		PageInfo paging = new PageInfo();
 		
+		authorMapper.deleteAll();
+		
+		assertThat(authorMapper.getCount(), is(0));
+		
+		authorMapper.authorEnroll(author1);
+		authorMapper.authorEnroll(author2);
+		
 		int total = authorMapper.authorGetTotal(paging);
 		
-		assertThat(6, is(total));
+		assertThat(total, is(2));;
 	}
 	
 	@Test
 	public void 작가_상세_정보_메서드_테스트() throws Exception {
-		int authorId = 1;
+		authorMapper.deleteAll();
 		
-		AuthorVO authorDetail = authorMapper.authorGetDetail(authorId);
+		assertThat(authorMapper.getCount(), is(0));
 		
-		assertThat("무지", is(authorDetail.getAuthorName()));
+		authorMapper.authorEnroll(author1);
+		
+		AuthorVO authorDetail = authorMapper.authorGetDetail(authorMapper.getLastPK());
+		
+		assertThat(author1.getAuthorName(), is(authorDetail.getAuthorName()));
 	}
 	
 	@Test
 	public void 작가_정보_수정_메서드_테스트() throws Exception {
-		AuthorVO modify = new AuthorVO();
+		authorMapper.deleteAll();
 		
-		modify.setAuthorId(2);
-		modify.setAuthorName("피카츄");
-		modify.setNationId("02");
-		modify.setAuthorProfile("수정된 내용입니다.");
+		assertThat(authorMapper.getCount(), is(0));
 		
-		int result = authorMapper.authorModify(modify);
+		authorMapper.authorEnroll(author1);
 		
-		assertThat(1, is(result));
+		int primaryKey = authorMapper.getLastPK();
+		
+		AuthorVO authorDetail = authorMapper.authorGetDetail(primaryKey);
+		
+		author2.setAuthorId(primaryKey);
+		
+		int result = authorMapper.authorModify(author2);
+		
+		AuthorVO ModifyDetail = authorMapper.authorGetDetail(primaryKey);
+		
+		assertThat(result, is(1));
+		assertNotEquals(authorDetail.getAuthorName(), ModifyDetail.getAuthorName());
 	}
 	
 	@Test
 	public void 작가_정보_삭제_메서드_테스트() throws Exception {
-		int result = authorMapper.authorDelete(26);
+		authorMapper.deleteAll();
 		
-		assertThat(1, is(result));
+		assertThat(authorMapper.getCount(), is(0));
+		
+		authorMapper.authorEnroll(author1);
+		
+		int primaryKey = authorMapper.getLastPK();
+		
+		int result = authorMapper.authorDelete(primaryKey);
+		
+		assertThat(result, is(1));
+		
+		AuthorVO authorDetail = authorMapper.authorGetDetail(primaryKey);
+		
+		assertNull(authorDetail);
 	}
 	
 	@Test
 	public void 작가_등록책수_출력_메서드_테스트() throws Exception {
-		int result = authorMapper.authorWrittenBook(27);
+		authorMapper.deleteAll();
 		
-		assertThat(0, is(result));
+		assertThat(authorMapper.getCount(), is(0));
+		
+		authorMapper.authorEnroll(author1);
+		
+		int primaryKey = authorMapper.getLastPK();
+		
+		int result = authorMapper.authorWrittenBook(primaryKey);
+		
+		assertThat(result, is(0));
 	}
-	
-	
 }
