77f41dec70ff501d2acce160da619cc6af03c876
youngho-j <alfkwl1239@naver.com>
2022-01-15 22:36:46 +0900

    수정 사항
    1. BookService 수정
     - 상품 수정 메서드 추가
     - 테스트 코드 추가

--- mvcPractice04/src/main/java/com/spring/shop/service/BookServiceImpl.java
+++ mvcPractice04/src/main/java/com/spring/shop/service/BookServiceImpl.java
@@ -74,17 +74,60 @@ public class BookServiceImpl implements BookService{
 
 	@Override
 	public BookVO goodsDetail(int bookId) throws Exception {
+		log.info("상품 상세정보 service 실행");
 		return bookMapper.goodsDetail(bookId);
 	}
-
+	
+	@Transactional
 	@Override
-	public int goodsModify(BookVO bookVO) {
-		return 0;
+	public int goodsModify(BookVO bookVO) throws Exception {
+		log.info("상품 수정 service 실행");
+		int updateResult = 0;
+		int imageUpdateResult = 0;
+		
+		ImageInfoVO imageInfo = null;
+		
+		// 상품 정보 수정
+		int bookUpdateResult = bookMapper.goodsModify(bookVO);
+		
+		// 상품 정보 수정 실패인 경우
+		if(bookUpdateResult < 1) {
+			log.info("상품 정보 등록실패!");
+			return 0;
+		}
+		
+		// 이미지 없는 상품 수정
+		if(bookUpdateResult == 1 && bookVO.getImagesList() == null || bookVO.getImagesList().size() <= 0) {
+			log.info("이미지 없는 상품 정보 수정");
+			return bookUpdateResult;
+		}
+		
+		// 기존 이미지 존재하는 경우
+		log.info("이미지 있는 상품 정보 등록");
+		log.info("기존 등록 이미지 삭제");
+		fileMapper.goodsImgDelete(bookVO.getBookId());
+		
+		log.info("새로운 이미지 등록");
+		for(int i = 0 ; i < bookVO.getImagesList().size() ; i++) {
+			imageInfo = new ImageInfoVO.Builder()
+					.bookId(bookVO.getBookId())
+					.uploadPath(bookVO.getImagesList().get(i).getUploadPath())
+					.uuid(bookVO.getImagesList().get(i).getUuid())
+					.fileName(bookVO.getImagesList().get(i).getFileName())
+					.build();
+					
+			imageUpdateResult += fileMapper.goodsImgEnroll(imageInfo);
+		}
+		
+		updateResult = bookUpdateResult + imageUpdateResult;
+		
+		return updateResult;
 	}
 	
 	@Transactional
 	@Override
 	public int goodsDelete(int bookId) throws Exception {
+		log.info("상품 삭제 service 실행");
 		int deleteResult = 0;
 		
 		List<ImageInfoVO> goodsImgList = fileMapper.getImageList(bookId);
--- mvcPractice04/src/test/java/com/spring/shop/service/BookServiceTest.java
+++ mvcPractice04/src/test/java/com/spring/shop/service/BookServiceTest.java
@@ -16,6 +16,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.spring.shop.mapper.AuthorMapper;
+import com.spring.shop.mapper.FileMapper;
 import com.spring.shop.vo.AuthorVO;
 import com.spring.shop.vo.BookVO;
 import com.spring.shop.vo.ImageInfoVO;
@@ -31,6 +32,9 @@ public class BookServiceTest {
 	private BookService bookService;
 	
 	@Autowired
+	private FileMapper fileMapper;
+	
+	@Autowired
 	private AuthorMapper authorMapper;
 	
 	private AuthorVO author1;
@@ -40,6 +44,7 @@ public class BookServiceTest {
 	private BookVO book3;
 	
 	private ImageInfoVO testImage1;
+	private ImageInfoVO testImage2;
 	
 	@Before
 	public void setUp() {
@@ -61,6 +66,12 @@ public class BookServiceTest {
 				.uploadPath("test\\2022\\01\\12")
 				.uuid("test")
 				.fileName("test").build();
+		
+		testImage2 = new ImageInfoVO
+				.Builder()
+				.uploadPath("test\\2022\\01\\14")
+				.uuid("tester")
+				.fileName("tester").build();
 		
 		List<ImageInfoVO> imageList = new ArrayList<ImageInfoVO>();
 		
@@ -115,7 +126,7 @@ public class BookServiceTest {
 	public void 이미지_없는_상품_등록_테스트() throws Exception {
 		assertThat(bookService.getCount(), is(0));
 		
-		int result = bookService.bookEnroll(book1);
+		int result = bookService.bookEnroll(book3);
 		
 		assertThat(result, is(1));
 		assertThat(bookService.getCount(), is(1));
@@ -177,5 +188,49 @@ public class BookServiceTest {
 		
 		assertThat(deleteResult, is(1));
 		assertThat(bookService.getCount(), is(0));
+	}
+	
+	@Test
+	public void 이미지없는_상품_수정_테스트() throws Exception {
+		assertThat(bookService.getCount(), is(0));
+		
+		bookService.bookEnroll(book1);
+		
+		int lastBookId = bookService.getLastPK();
+		
+		book3.setBookId(lastBookId);
+		
+		int updateResult = bookService.goodsModify(book3);
+		
+		BookVO updateDetail = bookService.goodsDetail(lastBookId);
+		
+		assertThat(updateResult, is(1));
+		assertThat(updateDetail.getBookName(), is(book3.getBookName()));
+	}
+	
+	@Test
+	public void 이미지있는_상품_수정_테스트() throws Exception {
+		assertThat(bookService.getCount(), is(0));
+		
+		bookService.bookEnroll(book1);
+		
+		int lastBookId = bookService.getLastPK();
+		
+		List<ImageInfoVO> imageList = new ArrayList<ImageInfoVO>();
+		
+		imageList.add(testImage2);
+		
+		book3.setBookId(lastBookId);
+		book3.setImagesList(imageList);
+		
+		int updateResult = bookService.goodsModify(book3);
+		
+		BookVO updateDetail = bookService.goodsDetail(lastBookId);
+		
+		ImageInfoVO updateImageInfo = fileMapper.getImageList(lastBookId).get(0);
+		
+		assertThat(updateResult, is(2));
+		assertThat(updateDetail.getBookName(), is(book3.getBookName()));
+		assertThat(updateImageInfo.getFileName(), is(testImage2.getFileName()));
 	}
 }
