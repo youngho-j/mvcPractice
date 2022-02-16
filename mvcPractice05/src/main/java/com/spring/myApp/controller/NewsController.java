5f2ba6d72de060020e7f426c6515ddc0f9eb07b5
youngho-j <alfkwl1239@naver.com>
2022-02-16 23:47:39 +0900

    MVC 웹 크롤러 만들기
    1. pom.xml 추가
     - spring version 5.2.6 업그레이드
     - java version 1.8 적용
     - java servlet 3.1 적용
       web.xml xmlns 버전 변경
     - 크롤링 위한 jsoup maven 등록
    
    2. log4j.xml 오류 해결
     - log4j.dtd 경로를 찾지 못해 에러 발생
       경로를 포함하여 수정
    
    2. NewsController 추가
     - 데이터 출력 테스트 위해 생성
       기존 newsURL로 jsoup 연결시 원하는 정보에 접근 불가
       기존 날짜설정을 하루전부터 오늘(22/2/16)으로 설정했음에도 불구하고 21년도 기사가 출력
       처음 검색시 URL 값 중 nso 파라미터 값이 인코딩되어 출력되는 현상을 보고 변조된 값을 넣어 검색시 원하는 값이 출력됨을 확인(Malform URL, HTTP 압축 등 한번 더 찾아 보기)
       브라우저에서 접속한 것처럼 header에 압축스킴 등 정보 추가
       원하는 정보 출력 확인

---
+++ mvcPractice05/src/main/java/com/spring/myApp/controller/NewsController.java
@@ -0,0 +1,71 @@
+package com.spring.myApp.controller;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class NewsController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(NewsController.class);
+	
+	@GetMapping(value = "/crawling")
+	public String newsCrawl() throws IOException {
+		DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy.MM.dd");
+		
+		String today = LocalDate.now().format(dateFormat);
+		
+		String yesterday = LocalDate.now().minusDays(1).format(dateFormat);
+		
+		String query = "올림픽";
+		
+		int startPage = 1;
+		
+			String newsURL = 
+					"https://search.naver.com/search.naver?"
+					+ "where=news&query=" + query
+					+ "&sm=tab_opt&sort=1&photo=0&field=0&pd=3"
+					+ "&ds=" + yesterday
+					+ "&de=" + today
+					+ "&docid=&related=0&mynews=0&office_type=0&office_section_code=0&news_office_checked="
+					+ "&nso=so:dd,p:from" + yesterday.replaceAll(".", "")
+					+ "to" + today.replaceAll(".", "") 
+					+ "&is_sug_officeid=0"
+					+ "&start=" + Integer.toString(startPage);
+			
+			Document rawDoc 
+				= Jsoup.connect(newsURL)
+					.userAgent("Mozilla/5.0 (Windows NT 10.0; win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36")
+					.header("Accept", "text/html")
+					.header("Accept-Encoding", "gzip,deflate")
+	                .header("Accept-Language", "it-IT,en;q=0.8,en-US;q=0.6,de;q=0.4,it;q=0.2,es;q=0.2")
+	                .header("Connection", "keep-alive")
+	                .ignoreContentType(true)
+					.timeout(5000).get();
+			
+			Elements elements = rawDoc.getElementsByClass("bx");
+			
+			for(Element element : elements) {
+				
+				String link = element.getElementsByClass("news_tit").attr("href");
+				String title = element.getElementsByClass("news_tit").attr("title");
+				
+				if(!link.isEmpty() && !title.isEmpty()) {
+					logger.info("링크 : {}", link);
+					logger.info("제목 : {}", title);
+				}
+			}
+			
+		
+		return "home";
+	}
+}
