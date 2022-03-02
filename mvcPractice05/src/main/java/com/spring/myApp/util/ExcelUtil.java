cc285f8cf2cca4429afff8955c9038947fdd3ade
youngho-j <alfkwl1239@naver.com>
2022-03-02 22:58:34 +0900

    MVC 웹 크롤러 만들기
    1. ExcelUtil 추가
     - Map 형태로 넘어온 데이터를 poi 활용하여 excel workbook 생성
     - jackson ObjectMapper 사용하여 news 목록을 List<LinckedHashMap<>> 에 담아 리턴
       unchecked 경고 표시 @SuppressWarnings 적용
     - 테스트 코드 작성
     - 추후 리팩토링 적용 필요함

---
+++ mvcPractice05/src/main/java/com/spring/myApp/util/ExcelUtil.java
@@ -0,0 +1,205 @@
+package com.spring.myApp.util;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.poi.common.usermodel.HyperlinkType;
+import org.apache.poi.hssf.usermodel.HSSFFont;
+import org.apache.poi.ss.usermodel.BorderStyle;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.Hyperlink;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.VerticalAlignment;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Component
+public class ExcelUtil {
+	
+	enum ExcelSizeOption {
+		ZERO((short) 0),
+		ONE((short) 1),
+		TWO((short) 2),
+		THREE((short) 3),
+		FOUR((short) 4),
+		FIVE((short) 5),
+		SIX((short) 6),
+		SEVEN((short) 7),
+		EIGHT((short) 8),
+		NINE((short) 9),
+		TEN((short) 10),
+		TWELVE((short) 12),
+		TWENTY((short) 20);
+		
+		private final short value;
+		
+		private ExcelSizeOption(short value) {
+			this.value = value;
+		}
+		
+		public short getValue() {return value;}
+	}
+	
+	public Workbook makeExcelFile(Map<String, Object> params) {
+		CellStyle style;
+		
+		int rowNum = 0;
+		
+		String keyword = params.get("keyword").toString();
+		
+		String selectOption;
+		
+		String convertOption = params.get("selectOption").toString();
+		
+		ObjectMapper mapper = new ObjectMapper();
+		
+		@SuppressWarnings("unchecked")
+		List<LinkedHashMap<String, String>> list = mapper.convertValue(params.get("newsList"), List.class);
+		
+		if(convertOption.equals("1")) {
+			 selectOption = "최신";
+		} else {
+			selectOption = "관련";
+		}
+		
+		Workbook workbook = new XSSFWorkbook();
+		
+		// 시트 생성
+		Sheet sheet = workbook.createSheet("Sheet1");
+		
+		// 제목 영역
+		// 제목 폰트
+		Font titleFont = workbook.createFont();
+		titleFont.setFontName(HSSFFont.FONT_ARIAL);
+		titleFont.setFontHeightInPoints(ExcelSizeOption.TWELVE.getValue());
+		titleFont.setBold(true);
+		titleFont.setColor(IndexedColors.AQUA.getIndex());
+					
+		// 셀 스타일 - 제목
+		style = workbook.createCellStyle();
+		style.setFont(titleFont);
+		style.setAlignment(HorizontalAlignment.CENTER);
+		style.setVerticalAlignment(VerticalAlignment.CENTER);
+		
+		// 제목 셀 생성
+		Cell titleCell = sheet.createRow(rowNum).createCell(0); // rowNum : 0
+		titleCell.setCellStyle(style);
+		titleCell.setCellValue(keyword + " " + selectOption + " 뉴스");
+		
+		// 셀 병합(제목 들어갈 셀)
+		sheet.addMergedRegion(
+					new CellRangeAddress(rowNum, rowNum, 0, 2));
+		
+		// 제목 영역 아래 빈행
+		sheet.createRow(++rowNum); // rowNum : 1
+		
+		// 테이블 헤더
+		// 테이블 헤더 폰트
+		Font tableHeaderFont = workbook.createFont();
+		tableHeaderFont.setFontName(HSSFFont.FONT_ARIAL);
+		tableHeaderFont.setFontHeightInPoints(ExcelSizeOption.TEN.getValue());
+		tableHeaderFont.setBold(true);
+		tableHeaderFont.setColor(IndexedColors.BLUE_GREY.getIndex());
+					
+		// 테이블 헤더 영역 셀 스타일 || 참고, 스타일 20 ~ 30회 이상 될 경우 스타일 적용되지 않는 버그 발생 - cellstyle 객체 재사용
+		style = workbook.createCellStyle();
+		style.setBorderBottom(BorderStyle.MEDIUM);
+		style.setBottomBorderColor(IndexedColors.ORANGE.getIndex());
+		style.setAlignment(HorizontalAlignment.CENTER);
+		style.setVerticalAlignment(VerticalAlignment.CENTER);
+		style.setFont(tableHeaderFont);
+		
+		// 테이블 헤더 영역(순서)
+		Cell tableHeaderNumber = sheet.createRow(++rowNum).createCell(0); // rowNum : 2
+		tableHeaderNumber.setCellStyle(style);
+		tableHeaderNumber.setCellValue("No");
+		
+		// 테이블 헤더 영역(제목)
+		Cell tableHeaderTitle;
+			
+		for(int i = 1 ; i < 7 ; i++) {
+			tableHeaderTitle = sheet.getRow(rowNum).createCell(i); // rowNum : 2
+			tableHeaderTitle.setCellStyle(style);
+			
+			if(i == 1) {
+				tableHeaderTitle.setCellValue("제목");							
+			}
+		}
+		
+		// 테이블 뉴스 제목 입력 칸 셀 병합
+		sheet.addMergedRegion(
+				new CellRangeAddress(rowNum, rowNum, 1, 6)); // rowNum : 2
+		
+		// 테이블 바디
+		// 테이블 바디 폰트
+		Font tableBodyFont = workbook.createFont();
+		tableBodyFont.setFontName(HSSFFont.FONT_ARIAL);
+		tableBodyFont.setFontHeightInPoints(ExcelSizeOption.EIGHT.getValue());
+		tableBodyFont.setBold(true);
+		
+		// 테이블 바디 순번 스타일
+		style = workbook.createCellStyle();
+		style.setAlignment(HorizontalAlignment.CENTER);
+		style.setVerticalAlignment(VerticalAlignment.CENTER);
+		style.setFont(tableBodyFont);
+		
+		// 테이블 바디 순번 셀 생성
+		Cell tableBodyNumber;
+		for(int i = 1 ; i <= list.size() ; i++) {
+			tableBodyNumber = sheet.createRow(i + rowNum).createCell(0); // rowNum : 2
+			tableBodyNumber.setCellStyle(style);
+			tableBodyNumber.setCellValue(i);			
+		}
+		
+		// 하이퍼링크 폰트
+		Font hyperlinkFont = workbook.createFont();
+		hyperlinkFont.setFontName(HSSFFont.FONT_ARIAL);
+		hyperlinkFont.setFontHeightInPoints(ExcelSizeOption.EIGHT.getValue());
+		hyperlinkFont.setUnderline(Font.U_SINGLE);
+		hyperlinkFont.setColor(IndexedColors.BLUE.getIndex());
+		hyperlinkFont.setBold(true);
+		
+		// 테이블 바디 하이퍼링크 스타일
+		style = workbook.createCellStyle();
+		style.setAlignment(HorizontalAlignment.CENTER);
+		style.setVerticalAlignment(VerticalAlignment.CENTER);
+		style.setFont(hyperlinkFont);
+		
+		// 테이블 바디 제목(링크)
+		Cell tableBodyLink;
+		// 테이블 바디 제목(링크) 첫줄 위치
+		++rowNum;
+		
+		for(int i = 0 ; i < list.size() ; i++) {
+			
+			for(int j = 1 ; j < 7 ; j++) {
+				tableBodyLink = sheet.getRow(i + rowNum).createCell(j); // rowNum : 3
+				
+				if(j == 1) {
+					tableBodyLink.setCellValue(list.get(i).get("newsTitle"));
+					Hyperlink link = workbook.getCreationHelper().createHyperlink(HyperlinkType.URL);
+					link.setAddress(list.get(i).get("newsURL"));
+					tableBodyLink.setHyperlink(link);
+				}
+				
+				tableBodyLink.setCellStyle(style);
+				
+			}
+			// 테이블 뉴스 제목 입력 칸 셀 병합
+			sheet.addMergedRegion(
+					new CellRangeAddress(i + rowNum, i + rowNum, 1, 6)); // rowNum : 3
+			
+		}
+		
+		return workbook;
+	}
+}
---
+++ mvcPractice05/src/test/java/com/spring/myApp/util/ExcelUtilTest.java
@@ -0,0 +1,56 @@
+package com.spring.myApp.util;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ExcelUtilTest {
+	
+	private ExcelUtil excel;
+	
+	private Map<String, Object> data;
+	
+	@Before
+	public void setUp() {
+		excel = new ExcelUtil();
+		
+		data = new HashMap<String, Object>();
+		
+		List<LinkedHashMap<String, String>> newsList = new ArrayList<LinkedHashMap<String,String>>();
+		
+		data.put("selectOption", "1");
+		data.put("keyword", "올림픽");
+		
+		LinkedHashMap<String, String> article = new LinkedHashMap<String, String>();
+		LinkedHashMap<String, String> article1 = new LinkedHashMap<String, String>();
+		
+		article.put("newsTitle", "네이버");
+		article.put("newsURL", "www.naver.com");
+		
+		article1.put("newsTitle", "구글");
+		article1.put("newsURL", "www.google.com");
+		
+		newsList.add(article);
+		newsList.add(article1);
+		
+		data.put("newsList", newsList);
+	}
+	
+	@Test
+	public void 엑셀_시트_생성_테스트() throws Exception {
+		
+		Workbook workbook = excel.makeExcelFile(data);
+		
+		assertFalse(workbook.getNumberOfSheets() == 0);
+		assertTrue(workbook.getSheetAt(0).getLastRowNum() > 0);
+	}
+
+}
